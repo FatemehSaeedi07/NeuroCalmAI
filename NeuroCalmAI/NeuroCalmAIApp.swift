696858298809440c2dc0309c97ea0e4830e52a01
Fatemeh <fatemehsaeedivelashani@gmail.com>
2024-09-22 13:34:03 -0500

    Initial Commit

---
+++ NeuroCalmAI/NeuroCalmAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeuroCalmAIApp.swift
+//  NeuroCalmAI
+//
+//  Created by Fatemeh Saeedi on 9/22/24.
+//
+
+import SwiftUI
+
+@main
+struct NeuroCalmAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
